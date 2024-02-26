ad5c594606a6d7e8912053cf342254090a1bb935
Thaigerhoo <jhhonathhan42@gmail.com>
2024-02-25 22:35:00 -0400

    hot fix

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - program that runs using printf and ends with a newline
+ * return 0 (Sucess)
+ */
+
+main(void)
+{
+printf("with proper grammar, but the outcome is a work of art,\n");
+return (0);
+}
---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the size of various types
+ * return 0 (Sucess)
+ */
+main(void)
+{
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
