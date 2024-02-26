37619f0ac08145253759f6aae7d3007559bc5941
Thaigerhoo <jhhonathhan42@gmail.com>
2024-02-25 21:33:13 -0400

    started programming

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main  - program that prints a message and is followed by a  new line
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
