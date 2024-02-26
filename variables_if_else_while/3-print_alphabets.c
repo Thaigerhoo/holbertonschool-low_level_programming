ff1621dbed4bf017f0d22df5c228bb5cf34e3f7a
Thaigerhoo <jhhonathhan42@gmail.com>
2024-02-26 13:48:18 -0400

    abc

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase and uppercase
+ * Return: 0 (Success)
+ */
 
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
