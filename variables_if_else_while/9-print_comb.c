196ecf55344391b511fccbcd9ba4482c6342ffc7
Thaigerhoo <jhhonathhan42@gmail.com>
2024-02-27 00:18:39 -0400

    fix

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,9 +8,11 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++){
+	for (num = 0; num < 10; num++)
+	{
 		putchar((num % 10) + '0');
-	if (num < 9){
+	if (num < 9)
+	{
 		putchar(',');
 	}
 	}
