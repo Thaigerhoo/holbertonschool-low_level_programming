89686d6407db8ec22887ad684ebb351d7c73310c
Thaigerhoo <jhhonathhan42@gmail.com>
2024-02-27 19:13:56 -0400

    fix

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds integers
+ * Return: sum of both
+ * @k: used for the sum
+ * @l: used for the sum
+ */
+
+int add(int k, int l)
+{
+	int j;
+
+	j = k + l;
+	return (j);
+}
