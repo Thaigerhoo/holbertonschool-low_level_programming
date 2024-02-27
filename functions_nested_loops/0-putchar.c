6ec7ce6911c657c8dedf60c1efc63f8d3e4bb062
Thaigerhoo <jhhonathhan42@gmail.com>
2024-02-27 11:57:51 -0400

    fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - printing program
  * Return: 0 (Success)
@@ -7,13 +7,13 @@
 int main(void)
 {
 	_putchar('_');
-	       	_putchar('p');
-	       	_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-	       	_putchar('a');
-		_putchar('r');
-		_putchar('\n');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 		return (0);
 }
