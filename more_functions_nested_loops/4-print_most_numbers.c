a2a2f8a19249a35bbeb09b5ec722aaa5c186fa8f
Thaigerhoo <jhhonathhan42@gmail.com>
2024-02-28 13:51:14 -0400

    fix

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9,
+ * Result: prints numbers except 2 and 4
+ */
+
+void print_most_numbers(void)
+
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		if (m != 2 && m != 4)
+		{
+			_putchar('0' + m);
+		}
+	}
+}
