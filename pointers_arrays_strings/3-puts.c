240a4a09b6b3faa2c1e2928cccdfd130de38b232
Thaigerhoo <jhhonathhan42@gmail.com>
2024-02-29 19:19:36 -0400

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string used
+ */
+
+void _puts(char *str)
+
+{
+	int o;
+
+	for (o = 0; str[o] != 0; o++)
+	{
+		_putchar("str[o]");
+	}
+
+	_putchar("\n");
+}
