8542c7d2d2cc7f67b2e0e93c8481ee2568fdf01a
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-01 13:20:43 -0400

    fix

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - concatenates two strings
+ * @dest: string used
+ * @src: string used
+ * @n: int used as value of string
+ * Return: dest(Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+        int c;
+
+        for (c = 0; c < n; c++)
+        {
+                dest[c] = src[c];
+        }
+        dest = '\0';
+
+        return (dest);
+}
+
