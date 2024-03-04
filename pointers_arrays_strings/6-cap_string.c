bfa29ea3f199ab4ad4ea5883c00e79c992afdc5b
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-03 23:32:52 -0400

    fix

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,13 +6,13 @@
  */
 
 char *cap_string(char *str)
-
 {
 	int i = 0;
 
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
 			i++;
 
 		if (str[i - 1] == ' ' ||
@@ -30,6 +30,8 @@ char *cap_string(char *str)
 	str[i - 1] == '}' ||
 	i == 0)
 			str[i] -= 32;
+		}
+
 		i++;
 	}
 
