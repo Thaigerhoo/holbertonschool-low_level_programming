18ba68c7200d897c54307844f937dac0a6d8bbae
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-04 15:18:17 -0400

    fix

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#define NULL 0
+/**
+ * _strpbrk - locates the first occurance in a string and another one
+ * @s: pointer used
+ * @accept: pointer to a char
+ * Return: NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
