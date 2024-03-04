1899714cfc4c9aeb76483a03c1aab7fa66bb2ae6
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-04 14:15:58 -0400

    fix

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#define NULL 0
+/**
+ * _strchr - returns a pointer to the first occurance of c in the string
+ * @s: pointer used
+ * @c: params to find
+ * Return: pointer to a character in the string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != 'c')
+		i++;
+
+	if (s[i] == c)
+
+		return (&s[i]);
+
+	else
+		return (NULL);
+
+}
