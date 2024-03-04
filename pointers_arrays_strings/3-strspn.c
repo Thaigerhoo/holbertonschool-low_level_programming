aba0cc243952bd37f6042961a191399fa63faed2
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-04 14:56:30 -0400

    fix

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: string used
+ * @accept: bytes of reference
+ * Return: nob (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+	int nob;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				nob++;
+				break;
+			}
+			if (accept[j + 1] != '\0' && s[i] != accept[j])
+				return (nob);
+		}
+		i++;
+	}
+	return (nob);
+}
