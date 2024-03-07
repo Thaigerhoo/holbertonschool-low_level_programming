7a7ad1f44be4ddd1a57a32e21e2ae0642cdd34a8
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-07 12:45:02 -0400

    advanced

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a line without using printf or puts
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (1);
+}
