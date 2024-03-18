be4a50f1a49e386cea16a98bfdf8fda39b5b8128
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-18 02:03:05 -0400

    fix

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * print_diagmus - sums two diagonals and prints the result
+ * @a: integers
+ * @size: size of the matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		s2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", s1, s2);
+}
