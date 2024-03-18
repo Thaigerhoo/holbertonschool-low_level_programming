fed03358420152305bd959b1d7d07f147bde9313
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-18 01:25:34 -0400

    fix

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,7 +8,7 @@ void print_chessboard(char (*a)[8])
 {
 	int row, line;
 
-	for (row = 0, a[row][7]; row++;)
+	for (row = 0; a[row][7]; row++)
 	{
 		for (line = 0; line < 8; line++)
 			_putchar(a[row][line]);
