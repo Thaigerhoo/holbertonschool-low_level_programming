795dc19baa543eeed89e6a4b3c8b9286fbbbdbc9
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-17 22:41:40 -0400

    fikx

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,33 +7,32 @@
 
 char *cap_string(char *str)
 {
-	int i = 0;
+        int i = 0;
 
-	while (str[i])
-	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-		{
-			i++;
+        while (str[i])
+        {
+                while (!(str[i] >= 'a' && str[i] <= 'z'))
 
-		if (str[i - 1] == ' ' ||
-	str[i - 1] == '\t' ||
-	str[i - 1] == '\n' ||
-	str[i - 1] == ',' ||
-	str[i - 1] == ';' ||
-	str[i - 1] == '.' ||
-	str[i - 1] == '!' ||
-	str[i - 1] == '?' ||
-	str[i - 1] == '"' ||
-	str[i - 1] == '(' ||
-	str[i - 1] == ')' ||
-	str[i - 1] == '{' ||
-	str[i - 1] == '}' ||
-	i == 0)
-			str[i] -= 32;
-		}
+                        i++;
 
-		i++;
-	}
+                if (str[i - 1] == ' ' ||
+        str[i - 1] == '\t' ||
+        str[i - 1] == '\n' ||
+        str[i - 1] == ',' ||
+        str[i - 1] == ';' ||
+        str[i - 1] == '.' ||
+        str[i - 1] == '!' ||
+        str[i - 1] == '?' ||
+        str[i - 1] == '"' ||
+        str[i - 1] == '(' ||
+        str[i - 1] == ')' ||
+        str[i - 1] == '{' ||
+        str[i - 1] == '}' ||
+        i == 0)
+                        str[i] -= 32;
 
-	return (str);
+                i++;
+        }
+
+        return (str);
 }
