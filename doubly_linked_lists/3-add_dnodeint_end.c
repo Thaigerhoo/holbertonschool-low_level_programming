063efc6791925676fe626dccf24082a4d5e28f03
Thaigerhoo <jhhonathhan42@gmail.com>
2024-03-27 15:24:19 -0400

    fixes

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,31 +1,41 @@
 #include "lists.h"
+
 /**
-* add_dnodeint_end - adds an element to the end of the list
-* @head: pointer to the pointer of the head
-* @n: const int n
-* Return: the address of the new element, NULL if failed
-*/
+ *  add_dnodeint_end - new node at the beginning of a dlistint_t list.
+ * @head: head
+ * @n : new data
+ * Return: the number of elements in a linked list
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *end;
+	dlistint_t *new_data, *last_node = *head;
 
-	if (!head)
+	new_data = malloc(sizeof(dlistint_t));
+
+	/*Handle error allocated memory*/
+	if (new_data == NULL)
 		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-	new->next = NULL;
-	new->prev = NULL;
-	new->n = n;
-	if (!*head)
-		*head = new;
-	else
+
+	/*Assing data*/
+	new_data->n = n;
+
+	if (*head == NULL)
 	{
-		end = *head;
-		while (end->next != NULL)
-			end = end->next;
-			end->next = new;
-			new->prev = end;
+		*head = new_data;
+		return (new_data);
 	}
-	return (new);
+	/*advance position*/
+	while (last_node->next != NULL)
+	{
+		last_node = last_node->next;
+	}
+
+	/*Last position*/
+	last_node->next = new_data;
+
+	/*Doubly linked list*/
+	new_data->next = NULL;
+	new_data->prev = last_node;
+
+	return (new_data);
 }
